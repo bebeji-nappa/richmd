f98535545a1aadb048c574c790dcb246367d1668
bebeji-nappa <masadecade@gmail.com>
2021-08-11 20:29:56 +0900

    Delete Dist

--- dist/parse/parser/helper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const EMPTY_REGEX = /^\s*$/;
-const isEmpty = (str) => {
-    return str.length === 0 || EMPTY_REGEX.test(str);
-};
-exports.default = {
-    isEmpty,
-};
