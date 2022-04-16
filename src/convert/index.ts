6bee3593dfb0dc8b073ffd4470635f76a22d40c9
bebeji-nappa <masadecade@gmail.com>
2022-04-16 21:07:46 +0900

    BugFix: Format KateX Error

--- src/convert/index.ts
+++ src/convert/index.ts
@@ -329,7 +329,7 @@ export const code = (data: OptionalConvert) => {
 };
 
 export const katex = (data: OptionalConvert) => {
-  const html = Katex.renderToString(String.raw`\displaystyle${data.values[0].value}`, {
+  const html = Katex.renderToString(String.raw`\textstyle ${data.values[0].value}`, {
     throwOnError: false,
   });
   return `<pre class="math katex-center">\n${html}\n</pre>\n`;
