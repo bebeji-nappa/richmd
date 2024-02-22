88aca08696182464cf57621bb862ef6b3266e807
bebeji-nappa <masadecade@gmail.com>
2024-02-22 21:44:57 +0900

    型定義修正

--- src/parse/index.ts
+++ src/parse/index.ts
@@ -2,7 +2,7 @@ import { parser } from "./parser";
 import Tree from "./Tree";
 
 export const parseMdTree = (mdString: string) => {
-  const ast = parser(mdString);
+  const ast: object[] & Convert[] = parser(mdString);
 
   return new Tree(ast);
 };
